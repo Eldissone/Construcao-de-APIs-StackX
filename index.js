35699cf0d8da81147808bbbe52a4033b74af6f62
Eldissone Vilonga <Evilonga@Uni01.local>
2024-10-25 22:25:52 +0100

    Resolvido conflitos

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-import express from 'express'
+import express, { json } from 'express';
+import Fs from 'fs';
 
 const app = express()
 app.use(express.json())
 
 const users = []
 
+
 app.post('/Usuarios', (req, res) => {
+    const { nome, idade, email, post } = req.body;
 
+    if (!nome || !idade || !email || !post) {
+        return res.status(400).send("Falta dados")
+    }
+
+    const data =  `Nome ${nome}, Idade ${idade}, email ${email}, post ${post}`
     users.push(req.body)
+    Fs.writeFileSync("arquivo.txt", JSON.stringify(users, null, 2))
+    Fs.appendFileSync("arquivo.txt", data)
 
-    res.status(201).json(req.body)
+    let result = Fs.readFileSync("arquivo.txt", "utf-8")
 
+    // Fs.unlinkSync("arquivo.txt")
+
+    console.log(result);
+    
+
+    // res.status(201).json(req.body)
+    res.status(201).send(data).json(req.body)
+    
 })
 
 
